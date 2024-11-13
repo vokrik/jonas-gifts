fa56f68fb8a53879321f134c6a931a09177dbb0b
Martin Vokrinek <martinvokrinek@Martins-MacBook-Air.local>
2024-11-13 17:20:12 +0100

    More gifts

--- pages/rozmazlujeme.js
+++ pages/rozmazlujeme.js
@@ -3,10 +3,10 @@ import ProductsPage from "@components/ProductsPage";
 const products = [
     {
         id: "koule",
-        image: "/koule.webp",
+        image: "/koule.png",
         title: "Blištivý kulébr",
         price: 800,
-        link: 'https://www.spokonozka.cz/hracky-a-hry/tickit-senzoricke-reflexni-barevne-kulicky-4-ks/?gad_source=1&gclid=Cj0KCQiAlsy5BhDeARIsABRc6ZuxzJikHPmOsZrscfsfZUomffhxoi1syyDEG4gsWrUQtCtMxeHEYAgaAn8YEALw_wcB'
+        link: 'https://www.utukutu.cz/senzoricke-hracky/barevne-tajuplne-senzoricke-koule/'
     }, {
         id: "kelimky",
         image: "/kelimky.png",
@@ -27,6 +27,13 @@ const products = [
         title: "Set knih",
         price: 900,
         link: 'https://www.martinus.cz/890659-chris-haughton-s-little-library/1740751?utm_source=google&utm_medium=cpc&utm_campaign=CZ-Martinus-D-PLA-tROAS-%CE%B1&utm_content=&utm_term=&utm_id=17439162855&gad_source=1&gclid=Cj0KCQiAlsy5BhDeARIsABRc6Zt8e8JWghyO1LwaPHrkPe7PmJRgceVGq1j_OIMJsNmoesAjXFACutEaAmyrEALw_wcB'
+    },
+    {
+        id: "nabytek",
+        image: "/nabytek.png",
+        title: "Multifunkční nábytek",
+        price: 2300,
+        link: 'https://www.bonami.cz/p/set-detskeho-nabytku-roba-2'
     }
 ]
 export default function DelameRadost() {
