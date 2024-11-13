59f6338ce93872e510d5432a5dd6856726a0a536
Martin Vokrinek <martinvokrinek@Martins-MacBook-Air.local>
2024-11-13 17:22:22 +0100

    Even more gifts

--- pages/rozmazlujeme.js
+++ pages/rozmazlujeme.js
@@ -25,7 +25,7 @@ const products = [
         id: "set-knih",
         image: "/set-knih.jpg",
         title: "Set knih",
-        price: 900,
+        price: 1200,
         link: 'https://www.martinus.cz/890659-chris-haughton-s-little-library/1740751?utm_source=google&utm_medium=cpc&utm_campaign=CZ-Martinus-D-PLA-tROAS-%CE%B1&utm_content=&utm_term=&utm_id=17439162855&gad_source=1&gclid=Cj0KCQiAlsy5BhDeARIsABRc6Zt8e8JWghyO1LwaPHrkPe7PmJRgceVGq1j_OIMJsNmoesAjXFACutEaAmyrEALw_wcB'
     },
     {
