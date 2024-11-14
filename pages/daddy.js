a54b174ebdc2d0d260babca5d9e2d204354abb6c
Martin Vokrinek <martinvokrinek@Martins-MacBook-Air.local>
2024-11-14 07:54:16 +0100

    price fix

--- pages/daddy.js
+++ pages/daddy.js
@@ -5,7 +5,7 @@ const products = [
         id: "jerab",
         image: "/jerab.webp",
         title: "Lego Jeřáb",
-        price: 1400,
+        price: 14000,
         link: 'https://www.alza.cz/hracky/lego-technic-42146-pasovy-jerab-liebherr-lr-13000-d7744563.htm?kampan=adwhr_hracky_pla_all_vendor-vykonnostni-lego_c_c_9219649__LO42146&gclid=Cj0KCQiAlsy5BhDeARIsABRc6Zu9Bc19VIiWbqCHvhyUCXa8AR2Exeu8hHGd1mMvAddSXJDI8LVl8JYaAiWpEALw_wcB'
     },
     {
