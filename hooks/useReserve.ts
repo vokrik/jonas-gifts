d159ed85e5a871e83309fb05537be1be051d4ba7
Martin Vokřínek <m.vokrinek@make.com>
2025-12-06 16:59:27 +0100

    Makes products connected to Make

--- hooks/useReserve.ts
+++ hooks/useReserve.ts
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 import type { ReservePayload } from '../types/api'
 
 const reserve = async (data: ReservePayload): Promise<void> => {
-    const response = await fetch('https://hook.eu2.make.com/yyybkkqvox9qrsqjmjlixj4n2rnknvmi', {
+    const response = await fetch('https://hook.eu2.make.com/stfvbey9vz8g7xbm4b9oi81tpfe2hp3i', {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
